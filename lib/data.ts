8bc2cb0442e033ce12a66c895aaa02a7c96c432c
Adarsh Chaudhary <chaudharyadarsh9919@gmail.com>
2025-09-10 15:58:33 +0530

    updated portfolio

--- lib/data.ts
+++ lib/data.ts
@@ -1,7 +1,10 @@
 import picmeAi from "@/public/picmeAi.png";
+import bombayVogue from "@/public/bombayVogue.png";
+import trendzila from "@/public/trendzila.png";
 
 import React from "react";
 import { CgWorkAlt } from "react-icons/cg";
+import { FaCode } from "react-icons/fa";
 
 export const links = [
   {
@@ -53,6 +56,43 @@ export const projectsData = [
 
     imageUrl: picmeAi,
   },
+  {
+    title: "Bombay Vogue",
+    description:
+      "Bombay Vogue is a premium men's apparel e-commerce store built on Shopify, offering stylish and high-quality clothing for the modern man. ",
+    tags: [
+      "Shopify",
+      "E-commerce",
+      "Menswear",
+      "Fashion",
+      "Apparel Store",
+      "SEO",
+      "UI/UX",
+      "Tailwind CSS v4",
+      "ShadCn UI",
+      "Responsive Design",
+    ],
+    projectUrl: "https://bombayvogue.in/",
+    imageUrl: bombayVogue,
+  },
+  {
+    title: "Trendzila",
+    description:
+      "Trendzila is a modern men's apparel e-commerce store delivering trendy, affordable, and stylish clothing for the fashion-forward man. Built on Shopify, ",
+    tags: [
+      "Shopify",
+      "E-commerce",
+      "Menswear",
+      "Fashion",
+      "Trendy Apparel",
+      "Affordable Clothing",
+      "SEO",
+      "UI/UX",
+      "Responsive Design",
+    ],
+    projectUrl: "https://trendzila.in/",
+    imageUrl: trendzila,
+  },
 ];
 
 export const skills = [
@@ -86,5 +126,13 @@ export const experiencesData = [
       "Freelanced as a web designer and Webflow specialist, building custom mockups and demos for potential clients, and later transitioned to web development for deeper technical growth.",
     icons: React.createElement(CgWorkAlt),
     date: "Oct,024 - Feb,2025",
+  },
+  {
+    title: "Web Developer",
+    location: "Lucknow, Uttar Pradesh, India",
+    description:
+      "Working as a freelance Web Developer, specializing in building responsive, SEO-friendly websites and web applications using modern technologies like React, Next.js, Tailwind CSS, and Shopify. ",
+    icons: React.createElement(FaCode), // from react-icons/fa
+    date: "Feb, 2025 – Present",
   },
 ];
